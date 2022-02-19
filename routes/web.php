d36ea8fe3f0ce6ddfde2314cfa6fe3bb355c2fbb
farkhad <farkhad@gmail.com>
2022-02-19 21:37:38 +0600

    40. Extract a Category Dropdown Blade Component
    Have you noticed that each route needs to pass a collection of categories to the posts view? If you take a look, that variable is only ever referenced as part of the main category dropdown. So with that in mind, what if we created a dedicated x-category-dropdown component that could be responsible for fetching any data that the dropdown requires? Let's figure out how to allow for that in this episode.

--- routes/web.php
+++ routes/web.php
@@ -24,13 +24,11 @@ Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts,
         'currentCategory' => $category,
-        'categories' => Category::all()
     ]);
 })->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
         'posts' => $author->posts,
-        'categories' => Category::all()
     ]);
 });
