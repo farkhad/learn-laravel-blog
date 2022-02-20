390aad738206fa51d1d458c3b0bf99f535cb4501
farkhad <farkhad@gmail.com>
2022-02-20 10:51:24 +0600

    41. Author Filtering
    Let's next add support for filtering posts according to their respective author. With this final piece of the puzzle, we'll now be able to easily sort all posts by category, or author, or search text, or all of the above.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Post;
 use App\Models\Category;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,14 +19,8 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function (Category $category) {
-    return view('posts', [
+    return view('posts.index', [
         'posts' => $category->posts,
         'currentCategory' => $category,
     ]);
 })->name('category');
-
-Route::get('authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'posts' => $author->posts,
-    ]);
-});
