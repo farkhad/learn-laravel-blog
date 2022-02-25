ca9f6a6ec3a377ce187e6caf0f1925d582680215
farkhad <farkhad@gmail.com>
2022-02-25 17:13:18 +0600

    50. Build the Log In Page
    We wrapped up the previous episode by creating a Log In link, but we stopped just short of building the view. Let's tackle that now.

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,9 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+
+            return redirect('/')->with('success', 'Welcome back!');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Provided credentials could not be verified.'
+        ]);
+        /*
+        return back()
+            ->withInput()
+            ->withErrors(['email' => 'Provided credentials could not be verified.']);
+        */
+    }
+
     public function destroy()
     {
         auth()->logout();
